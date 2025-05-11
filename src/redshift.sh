4da85875a68a0a28d85d92bb3473b372a1bd398e
9jh1 <tkf.x1os@gmail.com>
2025-05-11 13:16:54 +1200

    added redshift support

---
+++ src/redshift.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+base=6500 # default screen temp
+night=2500
+size=50
+step=$(((base-night)/size))
+echo $step
+while [ 1==1 ]; do 
+	time=$(date +%H)
+	current=$base 
+	if [ $time -gt 19 ];then 
+		echo "Starting Redshift"
+		redshift -x
+		for ((i = 0; i < size/3; i++)); do
+			redshift -o -O $((current-(step*(i+1))))
+			sleep 10
+		done
+	else
+		echo "Stopping Redshift"
+		redshift -x
+	fi
+	echo "Sleeping for 1 Minute"
+	sleep 60
+done
