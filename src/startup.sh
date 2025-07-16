f1fd750a1ce9e45fc80d298d9c79ab7449f8cd2d
9jh1 <tkf.x1os@gmail.com>
2025-07-16 19:18:40 +1200

    fixed bad picom startup, also added missing deps to list

--- src/startup.sh
+++ src/startup.sh
@@ -9,19 +9,16 @@ killall lxqt-policykit-agent
 killall plank
 
 "$HOME/.config/i3/src/screen.sh" &
-$HOME/.config/i3/src/wal.sh
+touch ~/.startup_log
+$HOME/.config/i3/src/wal.sh > ~/.startup_log
 
 
 #walltaker 
 $HOME/Pictures/Wallpapers/.walltaker/walltaker.sh --id "46554" &
 
-picom --config "$HOME/.config/i3/conf/picom.conf" --experimental-backends &
-snixembed &
-#picom -b --config "$HOME/.config/i3/conf/picom.conf" &
 nm-applet &
 blueman-applet &
 unclutter --timeout 0.5 &
 autotiling &
 lxqt-policykit-agent & 
-plank & 
 xset r rate 200 35 &
